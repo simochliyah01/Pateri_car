c56f5b1477204bf1a01953c7322546b7d314febe
simochliyah01 <simochliyah800@gmail.com>
2026-06-08 14:54:10 +0100

    chore(frontend): configure karma with ChromeHeadlessCI for pipeline runs

---
+++ frontend-client/karma.conf.js
@@ -0,0 +1,48 @@
+// Karma configuration
+// Works locally (Chrome) and in CI (ChromeHeadlessCI via --browsers flag)
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
+    jasmineHtmlReporter: { suppressAll: true },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/pateri-client'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--headless'
+        ]
+      }
+    },
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
